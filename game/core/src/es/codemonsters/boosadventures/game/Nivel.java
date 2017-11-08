47c00f44d6682a04ad0345cea481fb5990ca2908
Alejandro <a.sexto@codemonsters.es>
2017-11-08 17:15:10 +0100

    Actualizada definición de los niveles

--- game/core/src/es/codemonsters/boosadventures/game/Nivel.java
+++ game/core/src/es/codemonsters/boosadventures/game/Nivel.java
@@ -1,10 +1,32 @@
 package es.codemonsters.boosadventures.game;
 
+import com.badlogic.gdx.utils.Array;
+
+import es.codemonsters.boosadventures.game.objetosdeljuego.ObjetoDelJuego;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Nivel {
 
     private String archivo;
 
     public Nivel(String archivo) {
+        //TODO: Parsear el archivo JSON que recibe por argumento y a partir de él crear un array interno que contenga toda la inforamción leída
         this.archivo = archivo;
     }
+
+    public String getNombre() {
+        // TODO: Implementar
+        throw new NotImplementedException();
+    }
+
+    public String getSugerencia() {
+        // TODO: Implementar
+        throw new NotImplementedException();
+    }
+
+    public Array<ObjetoDelJuego> getObjetosDelJuego() {
+        //TODO: Devolver todos los ObjetoDelJuego encontrados
+        throw new NotImplementedException();
+    }
+
 }
