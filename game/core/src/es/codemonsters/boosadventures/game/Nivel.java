0679cda137420b1310d3b5341f330e97f283b9be
pathMarker <pathmarkerer@gmail.com>
2017-11-15 17:10:01 +0100

    Actualizado parser Json de Nivel
    Está mejor estructurado!!

--- game/core/src/es/codemonsters/boosadventures/game/Nivel.java
+++ game/core/src/es/codemonsters/boosadventures/game/Nivel.java
@@ -14,59 +14,40 @@ import es.codemonsters.boosadventures.game.objetosdeljuego.ObjetoDelJuego;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Nivel {
-
     private String archivo;
     private  World world;
+    JsonValue valoresJsonNivel;
     public Nivel(String archivo, World world) {
         //TODO: Parsear el archivo JSON que recibe por argumento y a partir de él crear un array interno que contenga toda la inforamción leída
         this.archivo = archivo;
         this.world = world;
+
+        JsonReader jsonRe = new JsonReader();
+        valoresJsonNivel = jsonRe.parse(Gdx.files.internal("niveles/" + archivo));
     }
 
     public String getNombre() {
-        // TODO: Implementar
-        throw new NotImplementedException();
+        String nombre = valoresJsonNivel.get("cabecera").getString("nombre");
+        return nombre;
     }
 
     public String getSugerencia() {
-        // TODO: Implementar
-        throw new NotImplementedException();
+        String sugerencia = valoresJsonNivel.get("cabecera").getString("sugerencia");
+        return sugerencia;
     }
 
     public Array<ObjetoDelJuego> getObjetosDelJuego() {
-
-        Array<ObjetoDelJuego> objetos = new Array<ObjetoDelJuego>();
-        Json json = new Json();
-        JsonReader jsonR = new JsonReader();
-        ArrayList<DataBlock> list = json.fromJson(ArrayList.class,DataBlock.class,Gdx.files.internal("niveles/"+archivo));
-        for (DataBlock v : list) {
-            if (v.clase.equals("Bloque")){
-                objetos.add(new Bloque(world,v.ancho,v.alto,v.x,v.y,v.angulo));
+        Array<ObjetoDelJuego> objetosDelJuegos = new Array<ObjetoDelJuego>();
+        JsonValue objetos = valoresJsonNivel.get("objetos");
+        for (JsonValue objeto: objetos) {
+            String clase = objeto.getString("clase");
+            if (clase.equals("bloque")) {
+                Bloque bloque = new Bloque(world,objeto.getFloat("ancho"),objeto.getFloat("alto"),objeto.getFloat("x"),objeto.getFloat("y"),objeto.getFloat("angulo"));
+                objetosDelJuegos.add(bloque);
+            } else {
+                Gdx.app.log("Nivel","Tipo de objeto desconocido: " + clase);
             }
         }
-
-
-        /*JsonReader json = new JsonReader();
-        JsonValue base = json.parse(Gdx.files.internal("niveles/001.json"));
-        System.out.println(base);
-        JsonValue component = base.get("objetos");
-        System.out.println(component.getString("clase"));
-        for (JsonValue componente : base.get("objetos"))
-        {
-            System.out.println(component.getString("clase"));
-        }*/
-        return objetos;
+        return objetosDelJuegos;
     }
-
-}
-class DataBlock {
-    public String nombre;
-    public String sugerencia;
-    public String clase;
-    public int x;
-    public int y;
-    public int ancho;
-    public int alto;
-    public int angulo;
-    public int regiontextura;
 }
